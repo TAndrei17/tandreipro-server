e8db70b19b988668a6f0a8fc5634173bed0cd85d
Andrei Trunkin <inboxavt@gmail.com>
2026-02-09 15:36:27 +0100

    feat: add route to fetch approved questions

--- src/routes/getQuestionsPublic.ts
+++ src/routes/getQuestionsPublic.ts
@@ -1,10 +1,9 @@
 import { Router, Request, Response } from 'express';
 
-// import pool from '@db/pool.js';
-
-import { QuestionsPublicResponse } from '@/types/Questions.js';
+import { QuestionsPublic, QuestionsPublicResponse } from '@/types/Questions.js';
 
 import routes from './routes.js';
+import pool from '../db/pool.js';
 
 const getQuestionsPublic = Router();
 
@@ -12,10 +11,25 @@ getQuestionsPublic.get(
 	routes.questionsPublic,
 	async (req: Request, res: Response<QuestionsPublicResponse>) => {
 		try {
-			return res.status(200).json({ success: true, message: '', data: [] });
+			const result = await pool.query(
+				`SELECT id, name, content, created_at
+			 FROM questions
+			 WHERE approved = $1
+			 `,
+				[true],
+			);
+
+			const data: QuestionsPublic[] = result.rows.map((row) => ({
+				id: row.id,
+				name: row.name,
+				content: row.content,
+				created_at: row.created_at.toISOString(),
+			}));
+
+			return res.status(200).json({ success: true, message: 'Data loaded successfully.', data });
 		} catch (error) {
 			console.error(error);
-			return res.status(500).json({ success: false, message: '' });
+			return res.status(500).json({ success: false, message: 'Unable to load data.' });
 		}
 	},
 );
