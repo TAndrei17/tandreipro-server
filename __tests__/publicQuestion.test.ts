226f0e9f89ec55ce35e42873c859c2c1308ff17c
Andrei Trunkin <inboxavt@gmail.com>
2026-02-10 20:13:03 +0100

    tests: update tests

--- __tests__/publicQuestion.test.ts
+++ __tests__/publicQuestion.test.ts
@@ -2,28 +2,28 @@ import 'dotenv/config';
 import express from 'express';
 import request from 'supertest';
 
-import postQuestion from '@/routes/postQuestion.js';
+import publicQuestions from '@/routes/publicQuestions.js';
 
 import pool from '../src/db/pool.js';
 
 const app = express();
 app.use(express.json());
-app.use(postQuestion);
+app.use(publicQuestions);
 
 afterAll(async () => {
-	await pool.query('DELETE FROM questions WHERE name = $1', ['Marty McFly']);
+	await pool.query('DELETE FROM questions');
 	await pool.end();
 });
 
-describe('POST /questions', () => {
-	it('returns 400 if data is missing', async () => {
+describe('all actions /public', () => {
+	it('POST/ returns 400 if data is missing', async () => {
 		const res = await request(app)
 			.post('/')
 			.send({ name: '', email: '', question: 'Some question' });
 		expect(res.status).toBe(400);
 	});
 
-	it('returns 201 if data is complete', async () => {
+	it('POST/ returns 201 if data is complete', async () => {
 		const res = await request(app).post('/').send({
 			name: 'Marty McFly',
 			email: 'backtothefuture@mail.org',
@@ -40,7 +40,7 @@ describe('POST /questions', () => {
 		expect(dbCheck.rowCount).toBe(1);
 	});
 
-	it('returns 500 if there is a server error', async () => {
+	it('POST/ returns 500 if there is a server error', async () => {
 		// Save the original method
 		const originalQuery = pool.query;
 
@@ -58,6 +58,42 @@ describe('POST /questions', () => {
 		expect(res.status).toBe(500);
 		expect(res.body.success).toBe(false);
 		expect(res.body.message).toBe('We’re sorry. We couldn’t receive your question.');
+
+		// Restore the original method
+		pool.query = originalQuery;
+	});
+
+	it('GET/ returns 200 if data is received', async () => {
+		await pool.query('UPDATE questions SET approved = $1 WHERE name = $2', [true, 'Marty McFly']);
+
+		const res = await request(app).get('/');
+		expect(res.status).toBe(200);
+		expect(res.headers['content-type']).toMatch(/json/);
+		expect(res.body.success).toBe(true);
+	});
+
+	it('GET/ returns 200 with empty array if no approved questions', async () => {
+		await pool.query('UPDATE questions SET approved = $1', [false]);
+
+		const res = await request(app).get('/');
+		expect(res.status).toBe(200);
+		expect(res.headers['content-type']).toMatch(/json/);
+		expect(res.body.success).toBe(true);
+		expect(res.body.data).toEqual([]);
+	});
+
+	it('GET/ returns 500 if there is a server error', async () => {
+		// Save the original method
+		const originalQuery = pool.query;
+
+		// Replace it with a function that throws an error
+		pool.query = async () => {
+			throw new Error('DB failure');
+		};
+
+		const res = await request(app).get('/');
+
+		expect(res.status).toBe(500);
 
 		// Restore the original method
 		pool.query = originalQuery;
