dd6fd3822ff29f3b0c790e72c33063628bcfa3c6
Andrei Trunkin <inboxavt@gmail.com>
2026-02-16 14:30:13 +0100

    feat: update routing

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,10 +2,13 @@ const routes = {
 	authLogin: '/login',
 	authLogout: '/logout',
 
-	public: '/',
+	public: '/questions',
+	publicTags: '/tags',
 
 	adminQuestions: '/questions',
 	adminQuestionOne: '/questions/:id',
+	adminTags: '/tags',
+	adminTag: '/tags/:id',
 };
 
 export default routes;
